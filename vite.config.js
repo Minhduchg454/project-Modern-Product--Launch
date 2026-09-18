d086181d20f96c1899c25a136b836db6cc014c8e
Nguyen Huu Duc <minhduchg454@gmail.com>
2026-09-18 13:39:05 +0700

    update page

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,23 @@ import { ViteEjsPlugin } from "vite-plugin-ejs";
 import path from "path";
 
 export default defineConfig({
-    base: process.env.NODE_ENV === "production" ? "/f8-zoom-module-1" : "/",
-    root: "src",
-    publicDir: "../public",
-    build: {
-        outDir: "../dist",
-        emptyOutDir: true,
-        rollupOptions: {
-            input: {
-                index: path.resolve(__dirname, "src/index.html"),
-                about: path.resolve(__dirname, "src/about.html"),
-                // ...
-            },
-        },
+  base:
+    process.env.NODE_ENV === "production"
+      ? "/project-Modern-Product--Launch/"
+      : "/",
+  root: "src",
+  publicDir: "../public",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "src/index.html"),
+        cart: path.resolve(__dirname, "src/cart.html"),
+        productDetail: path.resolve(__dirname, "src/productDetail.html"),
+        // ...
+      },
     },
-    plugins: [ViteEjsPlugin()],
+  },
+  plugins: [ViteEjsPlugin()],
 });
